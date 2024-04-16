306421a52591f0bd245e105d38f9026a8ce9af8c
Suhail Akhtar <suhailakhtar039@gmail.com>
2024-04-16 13:30:03 +0530

    modified structure

--- 01-Spring-Boot-Overview/My cool app/src/main/java/com/love2code/demo/My/cool/app/Controller/HelloWorld.java
+++ 01-Spring-Boot-Overview/My cool app/src/main/java/com/love2code/demo/My/cool/app/Controller/HelloWorld.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloWorld {
 
-    @GetMapping("/hi")
+    @GetMapping("/")
     public String index(){
         return "Hello world";
     }
