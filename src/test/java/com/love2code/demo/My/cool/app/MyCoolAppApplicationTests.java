fb3c2015a4db5841e9fb4ccdad75e3f415ead503
Suhail Akhtar <suhailakhtar039@gmail.com>
2024-04-16 10:49:29 +0530

    first commit

---
+++ src/test/java/com/love2code/demo/My/cool/app/MyCoolAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.love2code.demo.My.cool.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyCoolAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
