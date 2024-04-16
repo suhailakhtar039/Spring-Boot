a1c0f4dc05b858eb9a473ba3cfb687915f3541ad
Suhail Akhtar <suhailakhtar039@gmail.com>
2024-04-16 22:17:51 +0530

    using ${} for getting info from application file

--- 01-Spring-Boot-Overview/02-dev-tools-demo/src/main/java/com/love2code/demo/My/cool/app/Controller/HelloWorld.java
+++ 01-Spring-Boot-Overview/02-dev-tools-demo/src/main/java/com/love2code/demo/My/cool/app/Controller/HelloWorld.java
@@ -1,14 +1,20 @@
 package com.love2code.demo.My.cool.app.Controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloWorld {
+    @Value("${coach.name}")
+    private String coachName;
+
+    @Value("${team.name}")
+    private String teamName;
 
     @GetMapping("/")
     public String index(){
-        return "Hello world";
+        return "coach is " + coachName + " team is " + teamName;
     }
 
 //    expose new endpoint
