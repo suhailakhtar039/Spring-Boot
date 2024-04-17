a36e398943823e8637ff56336034448f51fe1c54
Suhail Akhtar <suhailakhtar039@gmail.com>
2024-04-17 19:38:33 +0530

    Same instance, singleton type

--- 02-spring-boot-spring-core/src/main/java/com/practice/springcoredemo/rest/DemoController.java
+++ 02-spring-boot-spring-core/src/main/java/com/practice/springcoredemo/rest/DemoController.java
@@ -10,16 +10,24 @@ import org.springframework.web.bind.annotation.RestController;
 public class DemoController {
     // define a private field for the dependency
     private Coach myCoach;
+    private Coach anotherCoach;
 
     // define constructor for the dependency injection
     @Autowired
-    public void DemoController(@Qualifier("cricketCoach")Coach theCoach){
+    public void DemoController(@Qualifier("cricketCoach") Coach theCoach,
+                               @Qualifier("cricketCoach") Coach anotherCoach) {
         System.out.println("In Constructor: " + getClass().getSimpleName());
         myCoach = theCoach;
+        this.anotherCoach = anotherCoach;
     }
 
     @GetMapping("/dailyworkout")
-    public String getDailyWorkout(){
+    public String getDailyWorkout() {
         return myCoach.getDailyWorkout();
+    }
+
+    @GetMapping("/check")
+    public String check(){
+        return "Comparing beans: myCoach==anotherCoach, " +(myCoach == anotherCoach);
     }
 }
