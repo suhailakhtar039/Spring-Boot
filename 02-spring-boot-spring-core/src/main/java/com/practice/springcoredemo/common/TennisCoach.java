9ad2f447bf75636b82c06795239224f5555af397
Suhail Akhtar <suhailakhtar039@gmail.com>
2024-04-17 17:07:05 +0530

    starting again

--- 02-spring-boot-spring-core/src/main/java/com/practice/springcoredemo/common/TennisCoach.java
+++ 02-spring-boot-spring-core/src/main/java/com/practice/springcoredemo/common/TennisCoach.java
@@ -1,10 +1,8 @@
 package com.practice.springcoredemo.common;
 
-import org.springframework.context.annotation.Lazy;
 import org.springframework.stereotype.Component;
 
 @Component
-@Lazy
 public class TennisCoach implements Coach {
 
     public TennisCoach(){
