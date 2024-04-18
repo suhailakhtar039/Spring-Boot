ca3aba5ee9e3100e55e6dfe29a1e193bf5edb4f6
Suhail Akhtar <suhailakhtar039@gmail.com>
2024-04-18 15:09:00 +0530

    studentDAO and StudentDAOimpl created

---
+++ 03-spring-boot-hibernate-jpa-crud/src/main/java/com/suhail/cruddemo/dao/StudentDAO.java
@@ -0,0 +1,7 @@
+package com.suhail.cruddemo.dao;
+
+import com.suhail.cruddemo.entity.Student;
+
+public interface StudentDAO {
+    void save(Student theStudent);
+}
---
+++ 03-spring-boot-hibernate-jpa-crud/src/main/java/com/suhail/cruddemo/dao/StudentDAOimpl.java
@@ -0,0 +1,25 @@
+package com.suhail.cruddemo.dao;
+
+import com.suhail.cruddemo.entity.Student;
+import jakarta.persistence.EntityManager;
+import jakarta.transaction.Transactional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class StudentDAOimpl implements  StudentDAO{
+
+    //define field for entity manager
+    private EntityManager entityManager;
+
+    @Autowired
+    public StudentDAOimpl(EntityManager entityManager){
+        this.entityManager = entityManager;
+    }
+
+    @Override
+    @Transactional
+    public void save(Student theStudent) {
+        entityManager.persist(theStudent);
+    }
+}
