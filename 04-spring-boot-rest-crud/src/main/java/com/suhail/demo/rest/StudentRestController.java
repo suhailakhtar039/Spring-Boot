faf7ccdee70cfab17271e35d8274dcdf19c9d727
Suhail Akhtar <suhailakhtar039@gmail.com>
2024-04-19 16:53:05 +0530

    custom error created Important

--- 04-spring-boot-rest-crud/src/main/java/com/suhail/demo/rest/StudentRestController.java
+++ 04-spring-boot-rest-crud/src/main/java/com/suhail/demo/rest/StudentRestController.java
@@ -2,10 +2,9 @@ package com.suhail.demo.rest;
 
 import com.suhail.demo.entity.Student;
 import jakarta.annotation.PostConstruct;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -17,20 +16,37 @@ public class StudentRestController {
     private List<Student> students;
 
     @PostConstruct
-    public void loadData(){
+    public void loadData() {
         students = new ArrayList<>();
-        students.add(new Student("suhail","akhtar"));
-        students.add(new Student("John","Wick"));
-        students.add(new Student("abc","xyz"));
+        students.add(new Student("suhail", "akhtar"));
+        students.add(new Student("John", "Wick"));
+        students.add(new Student("abc", "xyz"));
     }
 
     @GetMapping("/students")
-    public List<Student> getStudents(){
+    public List<Student> getStudents() {
         return students;
     }
 
     @GetMapping("/students/{studentId}")
-    public Student getStudentWithId(@PathVariable("studentId") int id){
-        return students.get(id);
+    public Student getStudentWithId(@PathVariable("studentId") int id) {
+
+        if (id < 0 || id >= students.size()){
+            throw new StudentsNotFoundException("Student id not found - " + id);
+        }
+
+            return students.get(id);
+    }
+
+    // Add exception handler using @ExceptionHandler
+    @ExceptionHandler
+    public ResponseEntity<StudentErrorResponse> handleException(StudentsNotFoundException exc){
+        StudentErrorResponse error = new StudentErrorResponse();
+
+        error.setStatus(HttpStatus.NOT_FOUND.value());
+        error.setMessage(exc.getMessage());
+        error.setTimeStamp(System.currentTimeMillis());
+
+        return new ResponseEntity<>(error ,HttpStatus.NOT_FOUND);
     }
 }
---
+++ 04-spring-boot-rest-crud/src/main/java/com/suhail/demo/rest/StudentsNotFoundException.java
@@ -0,0 +1,16 @@
+package com.suhail.demo.rest;
+
+public class StudentsNotFoundException extends RuntimeException {
+
+    public StudentsNotFoundException(String message) {
+        super(message);
+    }
+
+    public StudentsNotFoundException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public StudentsNotFoundException(Throwable cause) {
+        super(cause);
+    }
+}
