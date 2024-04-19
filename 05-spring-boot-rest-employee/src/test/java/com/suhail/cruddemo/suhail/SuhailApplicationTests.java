8a7a32c59824e9693fd7ad33db8c52299bd7a88b
Suhail Akhtar <suhailakhtar039@gmail.com>
2024-04-19 23:05:33 +0530

    created new project interacting with employee table

---
+++ 05-spring-boot-rest-employee/src/test/java/com/suhail/cruddemo/suhail/SuhailApplicationTests.java
@@ -0,0 +1,13 @@
+package com.suhail.cruddemo.suhail;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SuhailApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
