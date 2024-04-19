1181fdce0014f8c60829ae52500f0997d580414c
Suhail Akhtar <suhailakhtar039@gmail.com>
2024-04-19 22:19:31 +0530

    exception handler for bad request

--- 04-spring-boot-rest-crud/src/main/java/com/suhail/demo/rest/StudentRestController.java
+++ 04-spring-boot-rest-crud/src/main/java/com/suhail/demo/rest/StudentRestController.java
@@ -49,4 +49,15 @@ public class StudentRestController {
 
         return new ResponseEntity<>(error ,HttpStatus.NOT_FOUND);
     }
+    // adding generic exception handler
+    @ExceptionHandler
+    public ResponseEntity<StudentErrorResponse> handleException(Exception exc){
+        StudentErrorResponse error = new StudentErrorResponse();
+
+        error.setStatus(HttpStatus.BAD_REQUEST.value());
+        error.setMessage(exc.getMessage());
+        error.setTimeStamp(System.currentTimeMillis());
+
+        return new ResponseEntity<>(error ,HttpStatus.BAD_REQUEST);
+    }
 }
