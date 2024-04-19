4440e0aed9a8f530e551fe8a53ceb23b67526fd4
Suhail Akhtar <suhailakhtar039@gmail.com>
2024-04-19 23:22:28 +0530

    employee entity created

---
+++ 05-spring-boot-rest-employee/src/main/java/com/suhail/cruddemo/suhail/entity/Employee.java
@@ -0,0 +1,59 @@
+package com.suhail.cruddemo.suhail.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "employee")
+public class Employee {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    int id;
+
+    @Column(name = "first_name")
+    String firstName;
+
+    @Column(name = "last_name")
+    String latName;
+
+    @Column(name = "email")
+    String email;
+
+    public Employee(String firstName, String latName, String email) {
+        this.firstName = firstName;
+        this.latName = latName;
+        this.email = email;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLatName() {
+        return latName;
+    }
+
+    public void setLatName(String latName) {
+        this.latName = latName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
