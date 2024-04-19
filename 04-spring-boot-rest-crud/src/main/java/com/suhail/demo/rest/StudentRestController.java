abdbc2417ab1680048ea72ec49392570c111ceb2
Suhail Akhtar <suhailakhtar039@gmail.com>
2024-04-19 16:26:10 +0530

    postConstructor created

--- 04-spring-boot-rest-crud/src/main/java/com/suhail/demo/rest/StudentRestController.java
+++ 04-spring-boot-rest-crud/src/main/java/com/suhail/demo/rest/StudentRestController.java
@@ -1,7 +1,9 @@
 package com.suhail.demo.rest;
 
 import com.suhail.demo.entity.Student;
+import jakarta.annotation.PostConstruct;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,12 +14,23 @@ import java.util.List;
 @RequestMapping("/api")
 public class StudentRestController {
 
-    @GetMapping("/students")
-    public List<Student> getStudents(){
-        List<Student> students = new ArrayList<>();
+    private List<Student> students;
+
+    @PostConstruct
+    public void loadData(){
+        students = new ArrayList<>();
         students.add(new Student("suhail","akhtar"));
         students.add(new Student("John","Wick"));
         students.add(new Student("abc","xyz"));
+    }
+
+    @GetMapping("/students")
+    public List<Student> getStudents(){
         return students;
+    }
+
+    @GetMapping("/students/{studentId}")
+    public Student getStudentWithId(@PathVariable("studentId") int id){
+        return students.get(id);
     }
 }
