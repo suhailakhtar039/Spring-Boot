d4719d9ffad8fd6930797986a89f7355208904c1
Suhail Akhtar <suhailakhtar039@gmail.com>
2024-04-20 11:20:14 +0530

    refactored in controller to use service

--- 05-spring-boot-rest-employee/src/main/java/com/suhail/cruddemo/suhail/rest/EmployeeRestController.java
+++ 05-spring-boot-rest-employee/src/main/java/com/suhail/cruddemo/suhail/rest/EmployeeRestController.java
@@ -2,6 +2,7 @@ package com.suhail.cruddemo.suhail.rest;
 
 import com.suhail.cruddemo.suhail.dao.EmployeeDAO;
 import com.suhail.cruddemo.suhail.entity.Employee;
+import com.suhail.cruddemo.suhail.service.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,15 +13,15 @@ import java.util.List;
 @RestController
 @RequestMapping("/api")
 public class EmployeeRestController {
-    private EmployeeDAO employeeDAO;
+    private EmployeeService employeeService;
     @Autowired
-    public EmployeeRestController(EmployeeDAO employeeDAO){
-        this.employeeDAO = employeeDAO;
+    public EmployeeRestController(EmployeeService employeeService){
+        this.employeeService = employeeService;
     }
 
     @GetMapping("/employees")
     public List<Employee> getAll(){
-        return employeeDAO.findAll();
+        return employeeService.findAll();
     }
 
 }
