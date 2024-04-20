e522b83afff534140b02cc34830377ba4da0fcff
Suhail Akhtar <suhailakhtar039@gmail.com>
2024-04-20 23:22:45 +0530

    spring security added

--- 07-spring-boot-rest-employee -spring-data-rest/src/main/java/com/suhail/cruddemo/suhail/dao/EmployeeRepository.java
+++ 07-spring-boot-rest-employee -spring-data-rest/src/main/java/com/suhail/cruddemo/suhail/dao/EmployeeRepository.java
@@ -2,6 +2,8 @@ package com.suhail.cruddemo.suhail.dao;
 
 import com.suhail.cruddemo.suhail.entity.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+// @RepositoryRestResource(path = "members")
 public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
 }
