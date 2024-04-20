9439c35dda4938085b5e3e5f8af9a98fd3db8087
Suhail Akhtar <suhailakhtar039@gmail.com>
2024-04-20 13:39:00 +0530

    spring JPA started

---
+++ 06-spring-boot-rest-employee -spring-jpa/src/main/java/com/suhail/cruddemo/suhail/dao/EmployeeDAOJpaImpl.java
@@ -0,0 +1,44 @@
+package com.suhail.cruddemo.suhail.dao;
+
+import com.suhail.cruddemo.suhail.entity.Employee;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.TypedQuery;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public class EmployeeDAOJpaImpl implements EmployeeDAO {
+    private EntityManager entityManager;
+
+    @Autowired
+    public EmployeeDAOJpaImpl(EntityManager entityManager) {
+        this.entityManager = entityManager;
+    }
+
+    @Override
+    public List<Employee> findAll() {
+        TypedQuery<Employee> query = entityManager.createQuery("from Employee", Employee.class);
+        List<Employee> list = query.getResultList();
+        return list;
+    }
+
+    @Override
+    public Employee findById(int id) {
+        Employee employee = entityManager.find(Employee.class, id);
+        return employee;
+    }
+
+    @Override
+    public Employee save(Employee employee) {
+        Employee employeeSaved = entityManager.merge(employee);
+        return employeeSaved;
+    }
+
+    @Override
+    public void deleteById(int id) {
+        Employee employee = entityManager.find(Employee.class, id);
+        entityManager.remove(employee);
+    }
+}
