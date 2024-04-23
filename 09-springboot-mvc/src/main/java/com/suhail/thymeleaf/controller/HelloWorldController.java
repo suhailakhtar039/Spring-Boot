efaa60af1b2f98526ffc1c02280810f51df25521
Suhail Akhtar <suhailakhtar039@gmail.com>
2024-04-23 13:33:29 +0530

    hello world form created

---
+++ 09-springboot-mvc/src/main/java/com/suhail/thymeleaf/controller/HelloWorldController.java
@@ -0,0 +1,15 @@
+package com.suhail.thymeleaf.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HelloWorldController {
+
+    @RequestMapping("/showForm")
+    public String showForm(){
+        return "helloworld-form";
+    }
+
+    // @RequestMapping("")
+}
