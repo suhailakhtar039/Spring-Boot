22c5d3d1f1f27cabc9655bbbfddc16f1b8779cd5
Suhail Akhtar <suhailakhtar039@gmail.com>
2024-04-24 12:42:33 +0530

    student country added in model

--- 09-springboot-mvc/src/main/java/com/suhail/thymeleaf/model/Student.java
+++ 09-springboot-mvc/src/main/java/com/suhail/thymeleaf/model/Student.java
@@ -3,6 +3,7 @@ package com.suhail.thymeleaf.model;
 public class Student {
     private String firstName;
     private String lastName;
+    private String country;
 
     public Student(){}
 
@@ -25,5 +26,13 @@ public class Student {
 
     public void setLastName(String lastName) {
         this.lastName = lastName;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
     }
 }
