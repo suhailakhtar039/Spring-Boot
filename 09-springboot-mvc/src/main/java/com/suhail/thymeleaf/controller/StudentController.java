4d3df24de21de39788b5cf4b484ec9901f4a95b2
Suhail Akhtar <suhailakhtar039@gmail.com>
2024-04-24 12:56:09 +0530

    removed hardcoded values

--- 09-springboot-mvc/src/main/java/com/suhail/thymeleaf/controller/StudentController.java
+++ 09-springboot-mvc/src/main/java/com/suhail/thymeleaf/controller/StudentController.java
@@ -1,19 +1,26 @@
 package com.suhail.thymeleaf.controller;
 
 import com.suhail.thymeleaf.model.Student;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import java.util.List;
+
 @Controller
 public class StudentController {
-
+    @Value("${countries}")
+    private List<String> countries;
     @GetMapping("/showStudentForm")
     public String showForm(Model model){
         Student theStudent = new Student();
+
         model.addAttribute("student", theStudent);
+        model.addAttribute("countries", countries);
+
         return "student-form";
     }
 
