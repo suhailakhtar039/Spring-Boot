302e5cf0714c9dad46cf3e33dcee3889a407096c
Suhail Akhtar <suhailakhtar039@gmail.com>
2024-04-24 16:09:52 +0530

    spring boot mvc custom validation created

---
+++ 10-springboot-mvc-validation/src/test/java/com/suhail/custom/mvc/CustomvalidationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.suhail.custom.mvc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomvalidationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
