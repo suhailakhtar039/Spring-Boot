509738644e9508dc8dfd6ae18fb40f5d5498b4aa
Suhail Akhtar <suhailakhtar039@gmail.com>
2024-04-25 19:31:06 +0530

    CourseCodeConstraintValidator created for annotation

--- 10-springboot-mvc-validation/src/main/java/com/suhail/custom/mvc/validation/CourseCodeConstraintValidator.java
+++ 10-springboot-mvc-validation/src/main/java/com/suhail/custom/mvc/validation/CourseCodeConstraintValidator.java
@@ -1,4 +1,20 @@
 package com.suhail.custom.mvc.validation;
 
-public class CourseCodeConstraintValidator {
+import jakarta.validation.ConstraintValidator;
+import jakarta.validation.ConstraintValidatorContext;
+
+public class CourseCodeConstraintValidator implements ConstraintValidator<CourseCode, String> {
+
+    private String coursePrefix;
+
+    @Override
+    public void initialize(CourseCode courseCode) {
+        coursePrefix = courseCode.value();
+    }
+
+    @Override
+    public boolean isValid(String theCode, ConstraintValidatorContext constraintValidatorContext) {
+        boolean result = theCode.startsWith(coursePrefix);
+        return result;
+    }
 }
