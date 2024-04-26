61e3fbeb2c9e087ae07d6f7ccdf44a9a631b0c8e
Suhail Akhtar <suhailakhtar039@gmail.com>
2024-04-26 12:16:20 +0530

    made update employee api

--- 11-spring-boot-spring-mvc-crud-thymeleaf/00-spring-boot-spring-mvc-crud-starter-code/src/main/java/com/luv2code/springboot/thymeleaf/controller/EmployeeController.java
+++ 11-spring-boot-spring-mvc-crud-thymeleaf/00-spring-boot-spring-mvc-crud-starter-code/src/main/java/com/luv2code/springboot/thymeleaf/controller/EmployeeController.java
@@ -5,10 +5,7 @@ import com.luv2code.springboot.thymeleaf.service.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -41,5 +38,12 @@ public class EmployeeController {
     public String saveEmployee(@ModelAttribute("employee") Employee employee){
         employeeService.save(employee);
         return "redirect:/employees/list";
+    }
+
+    @GetMapping("/showFormForUpdate")
+    public String showFormForUpdate(@RequestParam("employeeId") int id, Model model){
+        Employee employee = employeeService.findById(id);
+        model.addAttribute("employee", employee);
+        return "employees/employee-form";
     }
 }
