1f499273db35228b57e442c50dce4c8445fbdf51
Suhail Akhtar <suhailakhtar039@gmail.com>
2024-04-26 11:53:18 +0530

    sorted by last name

--- 11-spring-boot-spring-mvc-crud-thymeleaf/00-spring-boot-spring-mvc-crud-starter-code/src/main/java/com/luv2code/springboot/thymeleaf/dao/EmployeeRepository.java
+++ 11-spring-boot-spring-mvc-crud-thymeleaf/00-spring-boot-spring-mvc-crud-starter-code/src/main/java/com/luv2code/springboot/thymeleaf/dao/EmployeeRepository.java
@@ -3,8 +3,12 @@ package com.luv2code.springboot.thymeleaf.dao;
 import com.luv2code.springboot.thymeleaf.entity.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
 
     // that's it ... no need to write any code LOL!
+
+    public List<Employee> findAllByOrderByLastNameAsc();
 
 }
