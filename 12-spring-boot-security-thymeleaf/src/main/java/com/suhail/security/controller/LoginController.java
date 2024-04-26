065b8a347599c4cfec68ef33e9a5cedd48f9ce88
Suhail Akhtar <suhailakhtar039@gmail.com>
2024-04-26 22:54:28 +0530

    custom login page created

--- 12-spring-boot-security-thymeleaf/src/main/java/com/suhail/security/controller/LoginController.java
+++ 12-spring-boot-security-thymeleaf/src/main/java/com/suhail/security/controller/LoginController.java
@@ -4,11 +4,11 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-public class DemoController {
+public class LoginController {
 
-    @GetMapping("/")
-    public String showHome(){
-        return "home";
+    @GetMapping("/showMyLoginPage")
+    public String showMyLoginPage(){
+        return "plain-login";
     }
 
 }
