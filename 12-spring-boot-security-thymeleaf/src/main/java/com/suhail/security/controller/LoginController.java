36ee10d95e8f8600d4bfd0ebebd1dc1d4180b2c9
Suhail Akhtar <suhailakhtar039@gmail.com>
2024-04-27 13:02:57 +0530

    directed controller to fancy login

--- 12-spring-boot-security-thymeleaf/src/main/java/com/suhail/security/controller/LoginController.java
+++ 12-spring-boot-security-thymeleaf/src/main/java/com/suhail/security/controller/LoginController.java
@@ -8,7 +8,8 @@ public class LoginController {
 
     @GetMapping("/showMyLoginPage")
     public String showMyLoginPage(){
-        return "plain-login";
+        // return "plain-login";
+        return "fancy-login";
     }
 
 }
