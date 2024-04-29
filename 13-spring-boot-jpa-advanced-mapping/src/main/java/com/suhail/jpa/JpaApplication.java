0e056ed09c9c6f95c84b18508bb771c754e3f64b
Suhail Akhtar <suhailakhtar039@gmail.com>
2024-04-29 23:02:20 +0530

    application working perfectly

--- 13-spring-boot-jpa-advanced-mapping/src/main/java/com/suhail/jpa/JpaApplication.java
+++ 13-spring-boot-jpa-advanced-mapping/src/main/java/com/suhail/jpa/JpaApplication.java
@@ -1,13 +1,22 @@
 package com.suhail.jpa;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class JpaApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(JpaApplication.class, args);
+	}
+
+	@Bean
+	public CommandLineRunner commandLineRunner(String[] args){
+		return runner -> {
+			System.out.println("Hello");
+		};
 	}
 
 }
