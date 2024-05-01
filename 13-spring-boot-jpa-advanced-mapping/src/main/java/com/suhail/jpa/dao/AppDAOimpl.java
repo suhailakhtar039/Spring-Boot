77cdb4dc5de8219f54dffa4818e1fb51cea60949
Suhail Akhtar <suhailakhtar039@gmail.com>
2024-05-01 23:12:37 +0530

    app dao created

--- 13-spring-boot-jpa-advanced-mapping/src/main/java/com/suhail/jpa/dao/AppDAOimpl.java
+++ 13-spring-boot-jpa-advanced-mapping/src/main/java/com/suhail/jpa/dao/AppDAOimpl.java
@@ -4,7 +4,9 @@ import com.suhail.jpa.entity.Instructor;
 import jakarta.persistence.EntityManager;
 import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public class AppDAOimpl implements  AppDAO{
     private EntityManager entityManager;
 
