2608c165b8f4c4ff3134058e1900063a16c11883
Suhail Akhtar <suhailakhtar039@gmail.com>
2024-05-05 22:39:11 +0530

    spring aop started

---
+++ 14-spring-aop/src/main/java/com/suhail/aop/AOP/AspectOrientedProgrammingApplication.java
@@ -0,0 +1,13 @@
+package com.suhail.aop.AOP;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AspectOrientedProgrammingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AspectOrientedProgrammingApplication.class, args);
+	}
+
+}
---
+++ 14-spring-aop/src/test/java/com/suhail/aop/AOP/AspectOrientedProgrammingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.suhail.aop.AOP;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AspectOrientedProgrammingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
