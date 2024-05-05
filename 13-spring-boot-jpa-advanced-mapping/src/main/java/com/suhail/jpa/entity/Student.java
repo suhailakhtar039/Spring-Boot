c6318d1b0b91715c74c015827e682c2fc8145a1c
Suhail Akhtar <suhailakhtar039@gmail.com>
2024-05-05 12:51:14 +0530

    student class completed for many-to-many

--- 13-spring-boot-jpa-advanced-mapping/src/main/java/com/suhail/jpa/entity/Student.java
+++ 13-spring-boot-jpa-advanced-mapping/src/main/java/com/suhail/jpa/entity/Student.java
@@ -2,6 +2,9 @@ package com.suhail.jpa.entity;
 
 import jakarta.persistence.*;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Entity
 @Table(name = "student")
 public class Student {
@@ -19,6 +22,14 @@ public class Student {
 
     @Column(name = "email")
     private String email;
+
+    @ManyToMany(cascade = {CascadeType.DETACH, CascadeType.PERSIST, CascadeType.MERGE, CascadeType.REFRESH}, fetch = FetchType.LAZY)
+    @JoinTable(
+            name = "course_student",
+            joinColumns = @JoinColumn(name = "student_id"),
+            inverseJoinColumns = @JoinColumn(name = "course_id")
+    )
+    private List<Course> courses;
 
     public Student() {
     }
@@ -59,6 +70,21 @@ public class Student {
 
     public void setEmail(String email) {
         this.email = email;
+    }
+
+    public List<Course> getCourses() {
+        return courses;
+    }
+
+    public void setCourses(List<Course> courses) {
+        this.courses = courses;
+    }
+
+    public void addCourse(Course course){
+        if(courses == null){
+            courses = new ArrayList<>();
+        }
+        courses.add(course);
     }
 
     @Override
