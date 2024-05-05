4636f71d2b993e47caac338bfcb5d0debd254560
Suhail Akhtar <suhailakhtar039@gmail.com>
2024-05-05 23:24:42 +0530

    accountDAO and impl created

---
+++ 14-spring-aop/src/main/java/com/suhail/aop/AOP/dao/AccountDAO.java
@@ -0,0 +1,5 @@
+package com.suhail.aop.AOP.dao;
+
+public interface AccountDAO {
+    void addAccount();
+}
---
+++ 14-spring-aop/src/main/java/com/suhail/aop/AOP/dao/AccountDAOimpl.java
@@ -0,0 +1,11 @@
+package com.suhail.aop.AOP.dao;
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class AccountDAOimpl implements AccountDAO{
+    @Override
+    public void addAccount() {
+
+    }
+}
