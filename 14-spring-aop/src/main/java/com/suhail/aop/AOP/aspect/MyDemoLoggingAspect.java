2d6de815b6585de16b6f399c1e415383e6431dde
Suhail Akhtar <suhailakhtar039@gmail.com>
2024-05-06 23:30:22 +0530

    return type can be anything but function starts with add

--- 14-spring-aop/src/main/java/com/suhail/aop/AOP/aspect/MyDemoLoggingAspect.java
+++ 14-spring-aop/src/main/java/com/suhail/aop/AOP/aspect/MyDemoLoggingAspect.java
@@ -8,7 +8,7 @@ import org.springframework.stereotype.Component;
 @Component
 public class MyDemoLoggingAspect {
 
-    @Before("execution(public void com.suhail.aop.AOP.dao.AccountDAO.addAccount())")
+    @Before("execution(* add*())")
     public void beforeAddAccountAdvice(){
         System.out.println("============> executing before advice on addAccount");
     }
