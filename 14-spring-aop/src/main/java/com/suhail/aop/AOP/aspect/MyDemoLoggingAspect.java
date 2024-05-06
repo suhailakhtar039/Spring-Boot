36a0f871185732aa5f6cd38a542a23f808f9e973
Suhail Akhtar <suhailakhtar039@gmail.com>
2024-05-06 17:12:50 +0530

    using only for account dao

--- 14-spring-aop/src/main/java/com/suhail/aop/AOP/aspect/MyDemoLoggingAspect.java
+++ 14-spring-aop/src/main/java/com/suhail/aop/AOP/aspect/MyDemoLoggingAspect.java
@@ -8,7 +8,7 @@ import org.springframework.stereotype.Component;
 @Component
 public class MyDemoLoggingAspect {
 
-    @Before("execution(public void addAccount())")
+    @Before("execution(public void com.suhail.aop.AOP.dao.AccountDAO.addAccount())")
     public void beforeAddAccountAdvice(){
         System.out.println("============> executing before advice on addAccount");
     }
