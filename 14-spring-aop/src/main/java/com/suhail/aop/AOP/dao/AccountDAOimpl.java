02affeba94086dc068ee6f15d21d5a83e2dfa5d2
Suhail Akhtar <suhailakhtar039@gmail.com>
2024-05-07 16:21:01 +0530

    getter and setter created along with some lines

--- 14-spring-aop/src/main/java/com/suhail/aop/AOP/dao/AccountDAOimpl.java
+++ 14-spring-aop/src/main/java/com/suhail/aop/AOP/dao/AccountDAOimpl.java
@@ -6,6 +6,9 @@ import org.springframework.stereotype.Repository;
 @Repository
 public class AccountDAOimpl implements AccountDAO{
 
+    private String name;
+    private String serviceCode;
+
     @Override
     public void addAccount(Account account, boolean vipFlag) {
         System.out.println(getClass() + ": DOING MY DB WORK: ADDING ACCOUNT");
@@ -15,5 +18,25 @@ public class AccountDAOimpl implements AccountDAO{
     public boolean doWork() {
         System.out.println(getClass() + " from AccountDaoImple");
         return false;
+    }
+
+    public String getName() {
+        System.out.println(getClass() + " within the get name");
+        return name;
+    }
+
+    public void setName(String name) {
+        System.out.println(getClass() + " with in the set name");
+        this.name = name;
+    }
+
+    public String getServiceCode() {
+        System.out.println(getClass() + " within the get service code");
+        return serviceCode;
+    }
+
+    public void setServiceCode(String serviceCode) {
+        System.out.println(getClass() + " with in the set service code");
+        this.serviceCode = serviceCode;
     }
 }
