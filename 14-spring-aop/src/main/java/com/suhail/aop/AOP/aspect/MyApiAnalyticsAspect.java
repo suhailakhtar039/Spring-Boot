988493a9bc8d014824917de17424ac1841fa5085
Suhail Akhtar <suhailakhtar039@gmail.com>
2024-05-07 23:30:14 +0530

    used join point in MyDemoLoggingAspect

--- 14-spring-aop/src/main/java/com/suhail/aop/AOP/aspect/MyApiAnalyticsAspect.java
+++ 14-spring-aop/src/main/java/com/suhail/aop/AOP/aspect/MyApiAnalyticsAspect.java
@@ -2,10 +2,12 @@ package com.suhail.aop.AOP.aspect;
 
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
+import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
 
 @Aspect
 @Component
+@Order(3)
 public class MyApiAnalyticsAspect {
     @Before("com.suhail.aop.AOP.aspect.AopExpression.forDaoPackageNoGetterSetter()")
     public void performApiAnalytics(){
--- 14-spring-aop/src/main/java/com/suhail/aop/AOP/aspect/MyDemoLoggingAspect.java
+++ 14-spring-aop/src/main/java/com/suhail/aop/AOP/aspect/MyDemoLoggingAspect.java
@@ -1,15 +1,22 @@
 package com.suhail.aop.AOP.aspect;
 
+import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.reflect.MethodSignature;
+import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
 
 @Aspect
 @Component
+@Order(2)
 public class MyDemoLoggingAspect {
     @Before("com.suhail.aop.AOP.aspect.AopExpression.forDaoPackageNoGetterSetter()")
-    public void beforeAddAccountAdvice(){
+    public void beforeAddAccountAdvice(JoinPoint joinPoint){
         System.out.println("============> executing before advice on addAccount");
+
+        MethodSignature methodSignature = (MethodSignature) joinPoint.getSignature();
+        System.out.println("Method: " + methodSignature);
     }
 
 }
