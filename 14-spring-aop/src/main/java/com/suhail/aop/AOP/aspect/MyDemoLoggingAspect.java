f7c7985a5848318e0675dd3562bec2837e59ce09
Suhail Akhtar <suhailakhtar039@gmail.com>
2024-05-08 23:14:43 +0530

    account name and level is null

--- 14-spring-aop/src/main/java/com/suhail/aop/AOP/aspect/MyDemoLoggingAspect.java
+++ 14-spring-aop/src/main/java/com/suhail/aop/AOP/aspect/MyDemoLoggingAspect.java
@@ -1,5 +1,6 @@
 package com.suhail.aop.AOP.aspect;
 
+import com.suhail.aop.AOP.Account;
 import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
@@ -12,11 +13,23 @@ import org.springframework.stereotype.Component;
 @Order(2)
 public class MyDemoLoggingAspect {
     @Before("com.suhail.aop.AOP.aspect.AopExpression.forDaoPackageNoGetterSetter()")
-    public void beforeAddAccountAdvice(JoinPoint joinPoint){
+    public void beforeAddAccountAdvice(JoinPoint joinPoint) {
         System.out.println("============> executing before advice on addAccount");
 
         MethodSignature methodSignature = (MethodSignature) joinPoint.getSignature();
         System.out.println("Method: " + methodSignature);
+
+        Object[] args = joinPoint.getArgs();
+
+        for (Object temp : args) {
+            System.out.println(temp);
+            if (temp instanceof Account) {
+                Account account = (Account) temp;
+                System.out.println("Account name: " + account.getName());
+                System.out.println("Account level: " + account.getLevel());
+            }
+        }
+
     }
 
 }
