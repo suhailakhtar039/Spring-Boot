04838c7e3380a84531af0ad9031dd314fef7eb12
Suhail Akhtar <suhailakhtar039@gmail.com>
2024-05-10 23:17:50 +0530

    multiple pointcut expressions created

--- 15-spring-boot-aop-mvc-crud-demo-employees/src/main/java/com/luv2code/springboot/thymeleafdemo/aspect/DemoLoggingAspect.java
+++ 15-spring-boot-aop-mvc-crud-demo-employees/src/main/java/com/luv2code/springboot/thymeleafdemo/aspect/DemoLoggingAspect.java
@@ -1,4 +1,26 @@
 package com.luv2code.springboot.thymeleafdemo.aspect;
 
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Pointcut;
+import org.springframework.stereotype.Component;
+
+import java.util.logging.Logger;
+
+@Aspect
+@Component
 public class DemoLoggingAspect {
+
+    private Logger myLogger = Logger.getLogger(getClass().getName());
+
+    @Pointcut("execution(* com.luv2code.springboot.thymeleafdemo.controller.*.*(..))")
+    private void forControllerPackage(){}
+
+    @Pointcut("execution(* com.luv2code.springboot.thymeleafdemo.service.*.*(..))")
+    private void forServicePackage(){}
+
+    @Pointcut("execution(* com.luv2code.springboot.thymeleafdemo.dao.*.*(..))")
+    private void forDaoPackage(){}
+
+    @Pointcut("forControllerPackage() || forServicePackage() || forDaoPackage()")
+    private void forAppFlow(){}
 }
